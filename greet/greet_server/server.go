cb5176bf7115b466a76ff0d0c1e8667a33fd40c3
Sumit Kumar <sumit.kumar@docquity.com>
2022-01-28 21:38:16 +0530

    feat:greet grpc server

---
+++ greet/greet_server/server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"grpc/greet/greetpb"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello world")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, greetpb.UnimplementedGreetServiceServer{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to server: %v", err)
+	}
+
+}
